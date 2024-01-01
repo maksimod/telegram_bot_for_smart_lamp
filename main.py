9b7f461fea8d0c9b598be74fb3e00f4de526ba35
maksimod <58116860+maksimod@users.noreply.github.com>
2024-01-01 11:01:57 +0300

    I was just testet a func

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-print("OK")
-print(3.14)
-print("FUCK")
+from dataclasses import dataclass
 
-#declaring super variable
-myvar : int = 5
+
+@dataclass
+class User:
+    user_id: int
+    name: str
+    age: int
+    email: str
+    
+    
+def get_user_info(user: User) -> str:
+    return f'Возраст пользователя {user.name} - {user.age}, ' \
+           f'а email - {user.email}'
+
+
+user_1: User = User(42, 'Vasiliy', 23, 'vasya_pupkin@pochta.ru')
+print(get_user_info(user_1))
